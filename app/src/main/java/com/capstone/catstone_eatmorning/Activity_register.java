94d2d2092a51d98d3e9c0286c51e016cbdf1c0ca
cdhcsh <cdhcsh@gmail.com>
2021-05-22 01:29:12 +0900

    address search

--- app/src/main/java/com/capstone/catstone_eatmorning/Activity_register.java
+++ app/src/main/java/com/capstone/catstone_eatmorning/Activity_register.java
@@ -2,13 +2,44 @@ package com.capstone.catstone_eatmorning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity_register extends AppCompatActivity {
-
-    @Override
+    EditText input_address;
+    private static final int SEARCH_ADDRESS_ACRIVITY = 10000;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        input_address = (EditText)findViewById(R.id.input_address);
+        initInput_address();
+    }
+    private void initInput_address(){
+        input_address = (EditText)findViewById(R.id.input_address);
+        Button btn_adress = (Button)findViewById(R.id.btn_address);
+        btn_adress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Activity_register.this,activity_address.class);
+                startActivityForResult(i,SEARCH_ADDRESS_ACRIVITY);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+                if(resultCode == RESULT_OK){
+                    String data = intent.getExtras().getString("data");
+                        input_address.setText(data);
+                }
     }
 }
---
+++ app/src/main/java/com/capstone/catstone_eatmorning/activity_address.java
@@ -0,0 +1,57 @@
+package com.capstone.catstone_eatmorning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class activity_address extends AppCompatActivity {
+    private WebView browser;
+
+    class MyJavaScriptInterface
+    {
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processDATA(String data) {
+            Bundle extra = new Bundle();
+            Intent intent = new Intent();
+            extra.putString("data", data);
+            intent.putExtras(extra);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+
+        browser = (WebView) findViewById(R.id.webView);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
+
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.loadUrl("javascript:sample2_execDaumPostcode();");
+            }
+        });
+
+        browser.loadUrl("http://52.231.164.5/daum.html");
+    }
+}
+
+
