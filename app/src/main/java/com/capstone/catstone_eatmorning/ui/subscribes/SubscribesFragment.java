351dfa48863e4e25a134d68ff53653b2881102f3
KangSeongHyeok <sung9820@naver.com>
2021-11-01 16:24:48 +0900

    sex

--- app/src/main/java/com/capstone/catstone_eatmorning/ui/subscribes/SubscribesFragment.java
+++ app/src/main/java/com/capstone/catstone_eatmorning/ui/subscribes/SubscribesFragment.java
@@ -1,10 +1,12 @@
 package com.capstone.catstone_eatmorning.ui.subscribes;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,17 +15,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.capstone.catstone_eatmorning.DataManager;
-import com.capstone.catstone_eatmorning.Member;
 import com.capstone.catstone_eatmorning.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class SubscribesFragment extends Fragment {
-    private TextView sub_user_name;
     private SubscribesViewModel slideshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -31,28 +25,6 @@ public class SubscribesFragment extends Fragment {
         slideshowViewModel =
                 new ViewModelProvider(this).get(SubscribesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_subscribes, container, false);
-
-        sub_user_name = (TextView)root.findViewById(R.id.sub_user_name);
-
-        DatabaseReference rootReference = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference UserReference;
-
-        UserReference = rootReference.child("users").child(DataManager.Logined_ID);
-        UserReference.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.d("Firebase", String.valueOf(task.getResult().getValue()));
-                } else {
-                    for (DataSnapshot d : task.getResult().getChildren()) {
-                        if (d.getKey().equals(Member.NAME)) {
-                            String string = String.valueOf(d.getValue());
-                            sub_user_name.setText(string);
-                        }
-                    }
-                }
-            }
-        });
         return root;
     }
 }
